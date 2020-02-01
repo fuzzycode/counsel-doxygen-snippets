6881fa1b44feaf2732109d04e48f0f4808925917
Björn Larsson <b.e.larsson@gmail.com>
2020-02-01 21:00:03 +0100

    Add code skeleton

---
+++ counsel-doxygen-snippets.el
@@ -0,0 +1,9 @@
+;;; counsel-doxygen-snippets.el --- Counsel frontend to search and insert references to doxygen code snippets. -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'counsel-doxygen-snippets)
+
+;;; counsel-doxygen-snippets.el ends here
