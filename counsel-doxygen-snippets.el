6e028d0769d10ea6bb96334e59a15e3bda8f31b5
Björn Larsson <b.e.larsson@gmail.com>
2020-02-02 09:31:47 +0100

    Add package header

--- counsel-doxygen-snippets.el
+++ counsel-doxygen-snippets.el
@@ -1,5 +1,33 @@
 ;;; counsel-doxygen-snippets.el --- Counsel frontend to search and insert references to doxygen code snippets. -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2020 Björn Larsson
+
+;; Author: Björn Larsson <develop@bjornlarsson.net>
+;; Homepage:
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "25.1")  (dash "2.17.0") (counsel "0.13.0"))
+;; Keywords: convenience, languages, tools
+
+;; This file is not part of GNU Emacs.
+
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+
+
 ;;; Commentary:
 
 ;;; Code:
